ae32347111ca56b84456727f78a05de8dadb594d
xiguaxigua <xiguaxigua888@gmail.com>
2021-12-26 11:46:41 +0800

    feat: eject config

--- src/components/code/index.tsx
+++ src/components/code/index.tsx
@@ -1,18 +1,38 @@
-import { FC, useEffect, useRef } from "react";
-import { editor } from "monaco-editor";
+import { FC } from "react";
+import MonacoEditor from "react-monaco-editor";
+
 import "./style.scss";
 
 const Code: FC<{}> = () => {
-  const codeEl = useRef(null);
-
-  useEffect(() => {
-    editor.create(codeEl.current!, {
-      value: "function hello() {\n\talert('Hello world!');\n}",
-      language: "javascript",
-    });
-  }, []);
-
-  return <div ref={codeEl} className="comp-code"></div>;
+  return (
+    <div className="comp-code">
+      <MonacoEditor
+        width="800"
+        height="600"
+        language="typescript"
+        theme="vs-dark"
+        value={`
+// First line
+function hello() {
+	alert('Hello world!');
+}
+// Last line`}
+        options={{
+          selectOnLineNumbers: true,
+          roundedSelection: false,
+          scrollBeyondLastLine: false,
+          readOnly: false,
+          theme: "vs-dark",
+        }}
+        onChange={(value) => {
+          console.log("value", value);
+        }}
+        editorDidMount={(editor) => {
+          editor.focus();
+        }}
+      />
+    </div>
+  );
 };
 
 export { Code };
