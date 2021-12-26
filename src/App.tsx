67a1604e9fcb3acdf61bbe39eccc15c683aec18d
xiguaxigua <xiguaxigua888@gmail.com>
2021-12-26 11:11:57 +0800

    feat: 引入 monaco-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.scss";
+import { Code } from "./components/code";
 
 function App() {
-  return <div className="app">app</div>;
+  return (
+    <div className="app">
+      <Code></Code>
+    </div>
+  );
 }
 
 export default App;
--- src/components/code/index.tsx
+++ src/components/code/index.tsx
@@ -1,7 +1,18 @@
+import { FC, useEffect, useRef } from "react";
+import { editor } from "monaco-editor";
 import "./style.scss";
 
-const Code = () => {
-  return <div className="comp-code">code</div>;
+const Code: FC<{}> = () => {
+  const codeEl = useRef(null);
+
+  useEffect(() => {
+    editor.create(codeEl.current!, {
+      value: "function hello() {\n\talert('Hello world!');\n}",
+      language: "javascript",
+    });
+  }, []);
+
+  return <div ref={codeEl} className="comp-code"></div>;
 };
 
 export { Code };
